bd308bd71aa4c20ba07c3b04b7498d78d5aafd0a
hulyamartli <hulyamartli@gmail.com>
2023-04-12 14:59:24 +0300

    if statement added for Enum shortage

--- src/Entities/Menu.java
+++ src/Entities/Menu.java
@@ -14,14 +14,22 @@ public class Menu {
     static List<Enum> beverages= new ArrayList<>(Arrays.asList(EBeverage.values()));
 
     public Menu(EDay day) {
-        this.soup = (ESoup) getItem(soups);
-        this.meal = (EMeal) getItem(meals);
-        this.desert = (EDesert) getItem(deserts);
-        this.beverage = (EBeverage) getItem(beverages);
+        this.soup = (ESoup) getItem(soups,ESoup.values());
+        this.meal = (EMeal) getItem(meals,EMeal.values());
+        this.desert = (EDesert) getItem(deserts,EDesert.values());
+        this.beverage = (EBeverage) getItem(beverages,EBeverage.values());
         this.day=day;
     }
 
-    public Enum getItem(List<Enum> list){
+    public Enum getItem(List<Enum> list,Enum[] enumList){
+        /*
+        * REASON FOR IF STATEMENT & enumList PARAMETER: e.g. If we would have less than 7 beverages in EBeverages, then the
+        * beverages won't be unique for each menu, but we won't get an exception. It will reassign a
+        * random value from the existing EBevarage values.
+         */
+        if(list.isEmpty()) {
+            list=Arrays.asList(enumList);
+        }
         Random rd = new Random();
         Enum en;
         en=list.get(rd.nextInt(list.size()));
